fee8640c3f50ff16add392d7218bf92e905ef48d
Dor-Yan <dorian.margraf@gmail.com>
2021-09-08 22:26:15 +0200

    Add project files.

---
+++ Codeally/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Codeally
+{
+    class Program
+    {
+
+        
+        static int Main()
+        {
+            
+            int a = 1;
+            int b = 1;
+
+            string value = Console.ReadLine();
+
+            CharEnumerator charEnumerator = value.GetEnumerator();
+
+                while (charEnumerator.MoveNext())
+                {
+                if (charEnumerator.Current == 'w') a++;
+                else if (charEnumerator.Current == 's') a--;
+                else if (charEnumerator.Current == 'a') b--;
+                else if (charEnumerator.Current == 'd') b++;
+                }
+
+            int movesToPoint = (7 - a) + (7 - b);
+            Console.WriteLine(movesToPoint);
+            return movesToPoint;
+
+        }
+    }
+}
